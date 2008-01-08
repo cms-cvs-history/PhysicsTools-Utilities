96931806234c8a70fee3ad7e190051edb91201b6
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2008-01-08 10:14:39 +0000

    added support for methods with one integer argument

--- test/testExpressionParser.cc
+++ test/testExpressionParser.cc
@@ -3,6 +3,8 @@
 #include "PhysicsTools/Utilities/interface/StringObjectFunction.h"
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/TrackReco/interface/TrackExtra.h"
+#include "DataFormats/Candidate/interface/CompositeCandidate.h"
+#include "DataFormats/Candidate/interface/LeafCandidate.h"
 #include <iostream>
 #include <Reflex/Object.h>
 #include <Reflex/Type.h>
@@ -19,7 +21,9 @@ public:
   void tearDown() {}
   void checkAll(); 
   void checkTrack(const std::string &, double);
+  void checkCandidate(const std::string &, double);
   reco::Track trk;
+  reco::CompositeCandidate cand;
   ROOT::Reflex::Object o;
   reco::parser::ExpressionPtr expr;
 };
@@ -27,20 +31,30 @@ public:
 CPPUNIT_TEST_SUITE_REGISTRATION(testExpressionParser);
 
 void testExpressionParser::checkTrack(const std::string & expression, double x) {
-  std::cerr << "parsing expression: \"" << expression << "\"" << std::endl;
+  std::cerr << "checking expression: \"" << expression << "\"" <<std::flush; 
   expr.reset();
   CPPUNIT_ASSERT(reco::parser::expressionParser<reco::Track>(expression, expr));
   CPPUNIT_ASSERT(expr.get() != 0);
   double res = expr->value(o);
-  std::cerr << expression << " = " << res << std::endl;
-  StringObjectFunction<reco::Track> f( expression );
+  StringObjectFunction<reco::Track> f(expression);
   CPPUNIT_ASSERT(fabs(f(trk) - res) < 1.e-6);
   CPPUNIT_ASSERT(fabs(f(trk) - x) < 1.e-6);
+  std::cerr << " = " << res << std::endl;
+}
+
+void testExpressionParser::checkCandidate(const std::string & expression, double x) {
+  std::cerr << "checking expression: \"" << expression << "\"" << std::flush; 
+  expr.reset();
+  CPPUNIT_ASSERT(reco::parser::expressionParser<reco::Candidate>(expression, expr));
+  CPPUNIT_ASSERT(expr.get() != 0);
+  double res = expr->value(o);
+  StringObjectFunction<reco::Candidate> f(expression);
+  CPPUNIT_ASSERT(fabs(f(cand) - res) < 1.e-6);
+  CPPUNIT_ASSERT(fabs(f(cand) - x) < 1.e-6);
+  std::cerr << " = " << res << std::endl;
 }
 
 void testExpressionParser::checkAll() {
-
-
   using namespace reco;
   using namespace ROOT::Reflex;
   const double chi2 = 20.0;
@@ -68,14 +82,31 @@ void testExpressionParser::checkAll() {
   edm::TestHandle<reco::TrackExtraCollection> h(&trkExtras, pid);
   reco::TrackExtraRef trkExtraRef(h, 0);
   trk.setExtra(trkExtraRef);
-  ROOT::Reflex::Type t = ROOT::Reflex::Type::ByTypeInfo(typeid(reco::Track));
-  o = ROOT::Reflex::Object(t, & trk);
-
-  checkTrack( "pt", trk.pt() );
-  checkTrack( "charge", trk.charge() );
-  checkTrack( "pt/3", trk.pt()/3 );
-  checkTrack( "momentum.x", trk.momentum().x() );
-  checkTrack( "hitPattern.numberOfValidHits", trk.hitPattern().numberOfValidHits() );
-  checkTrack( "extra.outerPhi", trk.extra()->outerPhi() );
-
+  {
+    ROOT::Reflex::Type t = ROOT::Reflex::Type::ByTypeInfo(typeid(reco::Track));
+    o = ROOT::Reflex::Object(t, & trk);
+    
+    checkTrack( "pt", trk.pt() );
+    checkTrack( "charge", trk.charge() );
+    checkTrack( "pt/3", trk.pt()/3 );
+    checkTrack( "momentum.x", trk.momentum().x() );
+    checkTrack( "hitPattern.numberOfValidHits", trk.hitPattern().numberOfValidHits() );
+    checkTrack( "extra.outerPhi", trk.extra()->outerPhi() );
+  }
+  reco::Candidate::LorentzVector p1(1, 2, 3, 4);
+  reco::Candidate::LorentzVector p2(1.1, 2.2, 3.3, 4.4);
+  reco::LeafCandidate c1(+1, p1);
+  reco::LeafCandidate c2(-1, p2);
+  cand.addDaughter(c1);
+  cand.addDaughter(c2);
+  CPPUNIT_ASSERT(cand.numberOfDaughters()==2);
+  CPPUNIT_ASSERT(cand.daughter(0)!=0);
+  CPPUNIT_ASSERT(cand.daughter(1)!=0);
+  {
+    ROOT::Reflex::Type t = ROOT::Reflex::Type::ByTypeInfo(typeid(reco::Candidate));
+    o = ROOT::Reflex::Object(t, & cand);  
+    checkCandidate( "numberOfDaughters", cand.numberOfDaughters() );
+    checkCandidate( "daughter(0).pt", cand.daughter(0)->pt() );
+    checkCandidate( "daughter(1).pt", cand.daughter(1)->pt() );
+  }
 }
