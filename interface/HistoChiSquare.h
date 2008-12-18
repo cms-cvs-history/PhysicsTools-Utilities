6f34e399ab9e8b34089316cab45b8f378de845cc
cmsbuild <>
2008-12-18 14:19:45 +0000

    This commit was manufactured by cvs2git to create tag 'PhysicsTools- Utilities-V00-08-07'.
    Sprout from master 2008-08-01 02:22:42 UTC Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch> 'test lone expression as cut and test error handling'
    Cherrypick from master 2008-12-18 14:19:44 UTC Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch> 'added combined chi-squared and likelihood fit':
        interface/CombinedChiSquaredLikelihood.h
        interface/Exception.h
        interface/Expression.h
        interface/HistoChiSquare.h
        interface/HistoPdf.h
        interface/Likelihood.h
        interface/MultiHistoChiSquare.h
        interface/RootFunctionHelper.h
        interface/RootMinuit.h
        interface/RootMinuitFuncEvaluator.h
        interface/RootMinuitResultPrinter.h
        interface/rootFunction.h
        interface/rootTf1.h
        test/testZMassFitExtLikelihood.cpp
        test/testZMassFitLikelihood.cpp

--- interface/HistoChiSquare.h
+++ interface/HistoChiSquare.h
@@ -1,7 +1,9 @@
 #ifndef PhysicsTools_Utilities_HistoChiSquare_h
 #define PhysicsTools_Utilities_HistoChiSquare_h
+#include "PhysicsTools/Utilities/interface/RootMinuitResultPrinter.h"
 #include <vector>
 #include "TH1.h"
+#include "TMath.h"
 
 namespace fit {
   template<typename T>
@@ -20,15 +22,15 @@ namespace fit {
       }
     }
     double operator()() const { 
-      double chi = 0;
+      double chi2 = 0;
       for(size_t i = 0; i < nBins_; ++i) { 
 	double x = xMin_ + ( i +.5 ) * deltaX_;
 	if((x > rangeMin_)&&(x < rangeMax_)&&(err_[i] > 0)) { 
 	  double r = ( cont_[i] - (*t_)(x) )/err_[i];
-	  chi += (r * r);
+	  chi2 += (r * r);
 	}
       }
-      return chi;
+     return chi2;
     }
     void setHistos(TH1 *histo) { 
       nBins_ = histo->GetNbinsX();
@@ -54,6 +56,16 @@ namespace fit {
     double xMin_, xMax_, deltaX_;
     std::vector<double> cont_;
     std::vector<double> err_;
+  };
+
+  template<typename T>
+  struct RootMinuitResultPrinter<HistoChiSquare<T> > {
+    static void print(double amin, unsigned int numberOfFreeParameters, const HistoChiSquare<T> & f) {
+      unsigned int ndof = f.degreesOfFreedom() - numberOfFreeParameters;
+      std::cout << "chi-squared/n.d.o.f. = " << amin << "/" << ndof << " = " << amin/ndof 
+		<< "; prob: " << TMath::Prob(amin, ndof)
+		<< std::endl;
+    }
   };
 }
 
